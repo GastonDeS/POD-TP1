fba8e23db8fa95c721f4f3a8d0b1ed77b5fe4050
florencia chao <fchao@itba.edu.ar>
2022-09-03 19:58:15 -0300

    feat: flight has ticket list

--- tpe1-g10/api/src/main/java/ar/edu/itba/pod/models/Flight.java
+++ tpe1-g10/api/src/main/java/ar/edu/itba/pod/models/Flight.java
@@ -2,17 +2,22 @@ package api.src.main.java.ar.edu.itba.pod.models;
 
 import api.src.main.java.ar.edu.itba.pod.constants.FlightStatus;
 
+import java.util.List;
+
 public class Flight {
     private final String plane;
     private final String code;
     private final String destination;
     private final FlightStatus status;
 
-    public Flight(String plane, String code, String destination, FlightStatus status) {
+    private final List<Ticket> ticketList;
+
+    public Flight(String plane, String code, String destination, FlightStatus status, List<Ticket> ticketList) {
         this.plane = plane;
         this.code = code;
         this.destination = destination;
         this.status = status;
+        this.ticketList = ticketList;
     }
 
     public String getPlane() {
@@ -29,5 +34,9 @@ public class Flight {
 
     public FlightStatus getStatus() {
         return status;
+    }
+
+    public void addTicketToFlight(Ticket ticket){
+        ticketList.add(ticket);
     }
 }
