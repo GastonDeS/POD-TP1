97d2054ebd7c8b3100df25559a727108ba4276d9
sol <skonfederak@itba.edu.ar>
2022-09-10 11:02:23 -0300

    chore: added exceptions on map service

--- tpe1-g10/api/src/main/java/ar/edu/itba/pod/interfaces/SeatMapServiceInterface.java
+++ tpe1-g10/api/src/main/java/ar/edu/itba/pod/interfaces/SeatMapServiceInterface.java
@@ -4,13 +4,14 @@ import api.src.main.java.ar.edu.itba.pod.constants.SeatCategory;
 import api.src.main.java.ar.edu.itba.pod.models.Seat;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.Map;
 
 public interface SeatMapServiceInterface extends Remote {
 
-    Map<String, Map<String, Seat>> peekAllSeats(String flightCode);
+    Map<String, Map<String, Seat>> peekAllSeats(String flightCode) throws RemoteException;
 
-    Map<String, Seat> peekRowSeats(String flightCode, String rowNumber);
+    Map<String, Seat> peekRowSeats(String flightCode, String rowNumber) throws RemoteException;
 
-    Map<String, Map<String, Seat>> peekCategorySeats(String flightCode, SeatCategory category);
+    Map<String, Map<String, Seat>> peekCategorySeats(String flightCode, SeatCategory category) throws RemoteException;
 }
