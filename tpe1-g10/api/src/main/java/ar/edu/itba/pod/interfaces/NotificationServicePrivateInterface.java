daa52d102c92090b2e5273ae394b49cfea8031ae
Juan <jdeluca@itba.edu.ar>
2022-09-10 13:49:03 -0300

    feat: added private interface for notification service

---
+++ tpe1-g10/api/src/main/java/ar/edu/itba/pod/interfaces/NotificationServicePrivateInterface.java
@@ -0,0 +1,13 @@
+package api.src.main.java.ar.edu.itba.pod.interfaces;
+
+import api.src.main.java.ar.edu.itba.pod.constants.NotificationCategory;
+import api.src.main.java.ar.edu.itba.pod.models.Ticket;
+
+import java.rmi.RemoteException;
+
+public interface NotificationServicePrivateInterface extends NotificationServiceInterface {
+
+    void newNotification(String flightNumber, String name, NotificationCategory notificationCategory) throws RemoteException;
+
+    void newNotificationUpdate(String flightNumber, String name, Ticket oldTicket, NotificationCategory notificationCategory) throws RemoteException;
+}
---
+++ tpe1-g10/api/src/test/java/ar/edu/itba/pod/services/NotificationServiceTest.java
@@ -0,0 +1,8 @@
+package api.src.test.java.ar.edu.itba.pod.services;
+
+import api.src.main.java.ar.edu.itba.pod.services.NotificationService;
+
+public class NotificationServiceTest {
+
+    private final NotificationService notificationService = NotificationService.getInstance();
+}
