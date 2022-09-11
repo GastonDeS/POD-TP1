0c51150aa2b59d24599eecd188868d9c9dd06241
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-09-10 23:08:16 -0300

    feat: run client admin sh

--- tpe1-g10/run-client.sh
+++ tpe1-g10/run-client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd `PWD`/client/target/tpe1-g10-client-1.0-SNAPSHOT
-chmod -R +x ./run-client.sh
-sh ./run-client.sh $*
+chmod -R +x ./run-admin.sh
+sh ./run-admin.sh -DserverAddress=127.0.0.1:1099 -Daction=MODELS -DinPath=/Users/gastondeschant/facultad/POD/POD-TP1/tpe1-g10/client/src/main/resources/filesCsv/planes.csv
+sh ./run-admin.sh -DserverAddress=127.0.0.1:1099 -Daction=FLIGHTS -DinPath=/Users/gastondeschant/facultad/POD/POD-TP1/tpe1-g10/client/src/main/resources/filesCsv/flights.csv
+
