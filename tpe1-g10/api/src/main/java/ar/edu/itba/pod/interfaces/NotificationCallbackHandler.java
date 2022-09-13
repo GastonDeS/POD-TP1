a9fea8146be7b5f90b23bbde015a70f2626b4cac
Juan <jdeluca@itba.edu.ar>
2022-09-12 22:16:29 -0300

    fix: moved notification logger to NotificationCallbackHandler

--- tpe1-g10/api/src/main/java/ar/edu/itba/pod/interfaces/NotificationCallbackHandler.java
+++ tpe1-g10/api/src/main/java/ar/edu/itba/pod/interfaces/NotificationCallbackHandler.java
@@ -8,17 +8,17 @@ import java.rmi.RemoteException;
 
 public interface NotificationCallbackHandler extends Remote {
 
-    void subscribedNotification(Logger logger, String flightCode, String dest) throws RemoteException;
+    void subscribedNotification(String flightCode, String dest) throws RemoteException;
 
-    void flightConfirmedNotification(Logger logger, String flightCode, String dest, String seatCategory, String place) throws RemoteException;
+    void flightConfirmedNotification(String flightCode, String dest, String seatCategory, String place) throws RemoteException;
 
-    void flightCancelledNotification(Logger logger, String flightCode, String dest, String seatCategory, String place) throws RemoteException;
+    void flightCancelledNotification(String flightCode, String dest, String seatCategory, String place) throws RemoteException;
 
-    void assignedSeatNotification(Logger logger, String flightCode, String dest, String seatCategory, String place) throws RemoteException;
+    void assignedSeatNotification(String flightCode, String dest, String seatCategory, String place) throws RemoteException;
 
-    void changedSeatNotification(Logger logger, String flightCode, String dest, String seatCategory, String place,
+    void changedSeatNotification(String flightCode, String dest, String seatCategory, String place,
                                  String oldSeatCategory, String oldPlace) throws RemoteException;
 
-    void changedTicketNotification(Logger logger, String flightCode, String dest, String seatCategory, String place,
+    void changedTicketNotification(String flightCode, String dest, String seatCategory, String place,
                                    String oldFlightCode, String oldDest) throws RemoteException;
 }
